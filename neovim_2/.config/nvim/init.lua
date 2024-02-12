836aa82daaa00ed70548937e5c15e4cb5489ed17
Federico Santander <federico.santander@bci.cl>
2024-02-12 10:46:18 -0300

    dap

--- neovim_2/.config/nvim/init.lua
+++ neovim_2/.config/nvim/init.lua
@@ -311,7 +311,7 @@ require("telescope").setup({
     help_tags = { theme = 'dropdown'},
   },
   file_sorter = require("telescope.sorters").get_fuzzy_file,
-  file_ignore_patterns = { "node_modules" },
+  file_ignore_patterns = { "node_modules", "package-lock.json" },
   generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
   extensions = {
     fzf = {
